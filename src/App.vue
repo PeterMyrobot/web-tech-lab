2fcf321464204300c304afc3bd4c207b6db011f8
Peter Yang <ipod4928@gmail.com>
2020-07-24 16:03:18 +0800

    Using customer theme for bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header />
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -16,11 +16,21 @@ export default {
 </script>
 
 <style lang="scss">
+@import "./style/main.scss";
+
+
+$colorSet1: #2d3142;
+$colorSet2: #bfc0c0;
+$colorSet3: #ffffff;
+$colorSet4: #ef8354;
+$colorSet5: #4f5d75;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
