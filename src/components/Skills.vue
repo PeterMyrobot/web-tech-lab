e1aff5d35b5d0968e1ff592795f04059d6356717
Peter Yang <ipod4928@gmail.com>
2021-06-13 01:00:29 +0800

    Finished skills section

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,11 +10,12 @@
         <div class="levelBar">
           <div class="level" :style="{ width: computeLevel(skill.level) }"></div>
         </div>
+        <div class="levelText"> {{skill.levelText}}</div>
       </div>
     </div>
-    <p class="description">
+    <!-- <p class="description">
       dlncldscnldscnlsdcmsdcmdm dmlcdlsc dlscscmscmdslcmsdcdcdscd
-    </p>
+    </p> -->
   </div>
 </template>
 
@@ -32,12 +33,24 @@ export default {
   data() {
     return {
       skills: [
-        { name: 'Vue.js', icon: vueIcon, level: 8 },
-        { name: 'React', icon: reactIcon, level: 7 },
-        { name: 'Electron', icon: electron, level: 6 },
-        { name: 'Python', icon: pythonIcon, level: 8 },
-        { name: 'git', icon: gitIcon, level: 7 },
-        { name: 'Raspi', icon: raspiIcon, level: 5 },
+        {
+          name: 'Vue.js', icon: vueIcon, level: 9, levelText: 'fluent',
+        },
+        {
+          name: 'React', icon: reactIcon, level: 8, levelText: 'expert',
+        },
+        {
+          name: 'Electron', icon: electron, level: 7, levelText: 'learned',
+        },
+        {
+          name: 'Python', icon: pythonIcon, level: 9, levelText: 'expert',
+        },
+        {
+          name: 'git', icon: gitIcon, level: 8, levelText: 'expert',
+        },
+        {
+          name: 'Raspi', icon: raspiIcon, level: 6, levelText: 'Proficient',
+        },
       ],
     };
   },
@@ -79,25 +92,28 @@ export default {
     flex-direction: column;
     // padding: 20px;
     justify-content: space-around;
+    min-width: 500px;
+    margin-left: 20px;
 
     .skillContainer {
       // border-radius: 2px;
-      height: 50px;
+      height: 30px;
       // width: 100px;
       // background-color: $colorSet3;
       // box-shadow: 1px 1px 3px;
+      margin-bottom: 10px;
       display: flex;
       flex-direction: row;
-      justify-content: center;
+      justify-content: end;
       align-items: center;
       img {
-        transition: ease-in-out 0.4s;
+        // transition: ease-in-out 0.4s;
         height: 30px;
         width: 30px;
         margin-right: 20px;
       }
       span {
-        transition: ease-in-out 0.4s;
+        // transition: ease-in-out 0.4s;
         display: none;
         cursor: default;
       }
@@ -110,15 +126,26 @@ export default {
       //   }
       // }
       .levelBar {
-        width: 40%;
-        height: 30px;
-        background-color: $colorSet2;
+        width: 400px;
+        height: 10px;
+        background-color: #000000;
         .level {
-          background-color: $colorSet5;
-          height: 30px;
+          background-color: $colorSet3;
+          height: 10px;
         }
       }
+      .levelText{
+        margin-left: 25px;
+        color: $colorSet5;
+        width:100px;
+        border:1px solid $colorSet5;
+        border-radius:2px;
+      }
     }
+  }
+  .description {
+    border: 1px solid #ffffff;
+    color: #ffffff;
   }
 }
 </style>
